43d9e4509716f53f2372139a1edcf6f4c1c00399
Yudha Romadhon <93903856+yudha130199@users.noreply.github.com>
2022-06-10 23:15:05 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Guide
+1. npm install
+2. npm run serve
---
+++ src/index.js
@@ -0,0 +1,18 @@
+require('file-loader?name=[name].[ext]!../public/index.html');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// const express = require('express');
+// import ReactDOMServer from 'react-dom/server';
+
+// const RenderNow  = ReactDOMServer.renderToString(<App />);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+, document.getElementById('root'));
+
+reportWebVitals();
